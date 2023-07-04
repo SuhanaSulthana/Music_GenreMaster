b100107b1be8cbc6f06f2f0a9918d5a209187e5d
Suhana Sulthana Shaik <79193646+SuhanaSulthana@users.noreply.github.com>
2023-07-04 14:34:25 +0530

    Update genre_master.py

--- genre_master.py
+++ genre_master.py
@@ -7,11 +7,16 @@ import random
 import operator
 
 
-def getNeighbors(trainingSet, instance, k):
+def calculate_distance(instance1, instance2, k):
+    distance = np.sum(np.abs(instance1[:k] - instance2[:k]))
+    return distance
+
+
+def get_neighbors(training_set, instance, k):
     distances = []
-    for x in range(len(trainingSet)):
-        dist = distance(trainingSet[x], instance, k) + distance(instance, trainingSet[x], k)
-        distances.append((trainingSet[x][2], dist))
+    for x in range(len(training_set)):
+        dist = calculate_distance(training_set[x], instance, k) + calculate_distance(instance, training_set[x], k)
+        distances.append((training_set[x][-1], dist))
     distances.sort(key=operator.itemgetter(1))
     neighbors = []
     for x in range(k):
@@ -19,73 +24,70 @@ def getNeighbors(trainingSet, instance, k):
     return neighbors
 
 
-def nearestClass(neighbors):
-    classVote = {}
-
-    for x in range(len(neighbors)):
-        response = neighbors[x]
-        if response in classVote:
-            classVote[response] += 1
+def get_majority_class(neighbors):
+    class_vote = {}
+    for neighbor in neighbors:
+        if neighbor in class_vote:
+            class_vote[neighbor] += 1
         else:
-            classVote[response] = 1
+            class_vote[neighbor] = 1
+    sorted_votes = sorted(class_vote.items(), key=operator.itemgetter(1), reverse=True)
+    return sorted_votes[0][0]
 
-    sorter = sorted(classVote.items(), key=operator.itemgetter(1), reverse=True)
-    return sorter[0][0]
 
-
-def getAccuracy(testSet, predictions):
-    correct = 0
-    for x in range(len(testSet)):
-        if testSet[x][-1] == predictions[x]:
-            correct += 1
-    return 1.0 * correct / len(testSet)
+def calculate_accuracy(test_set, predictions):
+    correct = sum(1 for x in range(len(test_set)) if test_set[x][-1] == predictions[x])
+    return correct / len(test_set)
 
 
 directory = "__path_to_dataset__"
-f = open("my.dat", 'wb')
+output_file = "my.dat"
 
-i = 0
+features = []
+class_index = 0
+
 for folder in os.listdir(directory):
-    i += 1
-    if i == 11:
+    class_index += 1
+    if class_index == 11:
         break
     for file in os.listdir(directory + folder):
         (rate, sig) = wav.read(directory + folder + "/" + file)
         mfcc_feat = mfcc(sig, rate, winlen=0.020, appendEnergy=False)
         covariance = np.cov(np.transpose(mfcc_feat))
         mean_matrix = mfcc_feat.mean(0)
-        feature = (mean_matrix, covariance, i)
-        pickle.dump(feature, f)
+        feature = (mean_matrix, covariance, class_index)
+        features.append(feature)
 
-f.close()
+with open(output_file, 'wb') as f:
+    pickle.dump(features, f)
 
 dataset = []
 
 
-def loadDataset(filename, split, trSet, teSet):
+def load_dataset(filename, split, training_set, test_set):
     with open(filename, 'rb') as f:
         while True:
             try:
                 dataset.append(pickle.load(f))
             except EOFError:
-                f.close()
                 break
 
-    for x in range(len(dataset)):
+    for data in dataset:
         if random.random() < split:
-            trSet.append(dataset[x])
+            training_set.append(data)
         else:
-            teSet.append(dataset[x])
+            test_set.append(data)
 
 
-trainingSet = []
-testSet = []
-loadDataset("my.dat", 0.66, trainingSet, testSet)
+training_set = []
+test_set = []
+load_dataset(output_file, 0.66, training_set, test_set)
 
-leng = len(testSet)
+test_set_length = len(test_set)
 predictions = []
-for x in range(leng):
-    predictions.append(nearestClass(getNeighbors(trainingSet, testSet[x], 5)))
+for x in range(test_set_length):
+    neighbors = get_neighbors(training_set, test_set[x], 5)
+    predictions.append(get_majority_class(neighbors))
 
-accuracy = getAccuracy(testSet, predictions)
+accuracy = calculate_accuracy(test_set, predictions)
 print("Accuracy:", accuracy)
